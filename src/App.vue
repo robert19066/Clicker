7f4d78b16b9f1bcf324dae34773b21f1397f8ca7
robert19066 <andronic.robert12oberteertt112@gmail.com>
2025-06-13 12:35:38 +0300

    V2 Overhaul

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,14 @@ import DataManager from './components/DataManager.vue';
 
 <template>
   <div class="app-container">
-    <h1 style="color: #eee; text-align: center;">Clicker Game</h1>
+    <h1 style="color: #eee; text-align: center;">Casual Vue Clicker</h1>
+    <h6 style="color: #ccc; text-align: center;">Made with love by <a href="https://github.com/robert19066" target="_blank">robert19066</a></h6>
+    <p>Welcome to the Casual Vue Clicker game! Click the button to earn points and upgrade your abilities.</p>
     <ClickerButton />
     <UpgradePanel />
     <DataManager />
+    <h4><a href="https://github.com/robert19066/Clicker" target="_blank">Open Repository</a></h4>
+    <h6 style="color: #ccc; text-align: center;">Made using Vue - Any forks will be accepted!</h6>
+    <h6><strong>Special thanks to Vue.js community!</strong></h6>
   </div>
 </template>
